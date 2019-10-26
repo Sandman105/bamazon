257f6a9ccdb196901c7376b03374e65cc8983590
Sandman105 <cpossandon@gmail.com>
2019-10-26 16:29:13 -0400

    table displays in terminal, two message prompts added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+
 //So from workbench we need to provide the connection information. This information is common for all languages, Java, C, etc. This returns a live connection and is stored in the variable. Database has to be created before you connect.
 var connection = mysql.createConnection({
     host: "localhost",
@@ -25,12 +26,42 @@ connection.connect(function (err) {
     startDisplay();
 });
 
+//Currently displaying table of items that are currently in stock
 function startDisplay() {
-    inquirer
-    .prompt (
-        {
+    console.log("Test");
 
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.table(res);
+        
+        
+        
+        inquirer
+        .prompt ([
+            
+            //COMPLETE: We need to prompt the user for the id they'd like to choose, so we need to use the inquirer module here.
+            {
+                name: "choice",
+                message: "What is the id of the product you'd like to buy?",
+                
+            },
+            //COMPLETE: Need a message asking the user "How many units of the product would you like to buy?"
+            
+            {
+                name: "choice",
+                message: "How many units of the product would you like to buy?"
+            }
+            
+        ])
+        //console.log();
+    });
 
-        }
-        )
+    
+
 }
+
+//function anotherDisplay() {
+    //connection.end();
+
+//}
